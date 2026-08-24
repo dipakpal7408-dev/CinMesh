5a1fcd792c471daea7d27dfc17d32de5a714ed86
Dipak <dipakpal7408@gmail.com>
2026-08-24 10:34:18 +0530

    Create api.js

---
+++ client/src/services/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("cinmesh_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("cinmesh_token");
+      localStorage.removeItem("cinmesh_user");
+      if (!window.location.pathname.startsWith("/login")) {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
